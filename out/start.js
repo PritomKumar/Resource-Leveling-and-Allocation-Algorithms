8465129303479c8665c1b4470cc937f520fd1b09
PritomKumar <bsse0919@iit.du.ac.bd>
2020-12-27 01:39:07 +0600

    read file sucessful

--- out/start.js
+++ out/start.js
@@ -1,13 +1,36 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
 class Activity {
-    constructor(name, resource, duration, description) {
-        this.name = name;
+    constructor(name = "", resource = 0, duration = 0) {
+        this.name = "";
+        this.description = "";
+        this.resource = 0;
+        this.duration = 0;
+        //Has to solve activity issue
+        //prevActivity:Activity = new Activity();
+        //nextActivity: Array<Activity> = [];
+        this.earlyStart = 0;
+        this.earlyFinish = 0;
+        this.lateStart = 0;
+        this.lateFinish = 0;
+        this.totalFloat = 0;
+        this.name = name.toLowerCase();
         this.duration = duration;
         this.resource = resource;
-        this.description = description;
     }
 }
+var a = new Activity("A", 3, 3);
 let str = "sd a  dfs sdf ";
-console.log(str);
+try {
+    // var data = fs.readFileSync("F:\\IIT 8th Semester\\Software Project Management\\Group Assingments\\Project 1\\src\\input.txt", 'utf8');
+    var data = fs_1.default.readFileSync("src\\input.txt", "utf8");
+    console.log(data);
+}
+catch (e) {
+    console.log("Error:", e.stack);
+}
 //# sourceMappingURL=start.js.map
--- src/start.ts
+++ src/start.ts
@@ -1,33 +1,35 @@
 export {};
-import fs from 'fs';
+import fs from "fs";
 
 class Activity {
     name: string = "";
     description: string = "";
     resource: number = 0;
     duration: number = 0;
-    prevActivity:Activity = new Activity();
-    nextActivity:Array<Activity> = [];
+    //Has to solve activity issue
+    //prevActivity:Activity = new Activity();
+    //nextActivity: Array<Activity> = [];
     earlyStart: number = 0;
     earlyFinish: number = 0;
     lateStart: number = 0;
     lateFinish: number = 0;
     totalFloat: number = 0;
 
-    constructor(
-        name: string = "",
-        resource: number = 0,
-        duration: number = 0
-    ) {
+    constructor(name: string = "", resource: number = 0, duration: number = 0) {
         this.name = name.toLowerCase();
         this.duration = duration;
         this.resource = resource;
     }
 }
 
-var a = new Activity("A",3,3);
+var a = new Activity("A", 3, 3);
 
 let str = "sd a  dfs sdf ";
 
-var input = fs.readFileSync('input.txt','utf8');
-console.log(str);
+try {
+    // var data = fs.readFileSync("F:\\IIT 8th Semester\\Software Project Management\\Group Assingments\\Project 1\\src\\input.txt", 'utf8');
+    var data = fs.readFileSync("src\\input.txt", "utf8");
+    console.log(data);
+} catch (e) {
+    console.log("Error:", e.stack);
+}
