f7cdcd3961abaa02f6b3447b3795c33ea23b7d80
PritomKumar <bsse0919@iit.du.ac.bd>
2020-12-27 00:48:54 +0600

    Creating activity

--- out/start.js
+++ out/start.js
@@ -1,5 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class Activity {
+    constructor(name, resource, duration, description) {
+        this.name = name;
+        this.duration = duration;
+        this.resource = resource;
+        this.description = description;
+    }
+}
 let str = "sd a  dfs sdf ";
 console.log(str);
 //# sourceMappingURL=start.js.map
--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,28 @@
-export{}
+export {};
 
-let str  = "sd a  dfs sdf "; 
+class Activity {
+    name: string;
+    description: string;
+    resource: number;
+    duration: number;
+    earlyStart: number = 0;
+    earlyFinish: number = 0;
+    lateStart: number = 0;
+    lateFinish: number = 0;
+    totalFloat: number = 0;
+
+    constructor(
+        name: string,
+        resource: number,
+        duration: number,
+        description: string
+    ) {
+        this.name = name;
+        this.duration = duration;
+        this.resource = resource;
+        this.description = description;
+    }
+}
+
+let str = "sd a  dfs sdf ";
 console.log(str);
