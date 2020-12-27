16ac8bf243f730bbac0cb805228a9aedd117a7b9
PritomKumar <bsse0919@iit.du.ac.bd>
2020-12-27 08:56:47 +0600

    readfile line by line

--- out/start.js
+++ out/start.js
@@ -1,11 +1,27 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 class Activity {
-    constructor(name = "", resource = 0, duration = 0) {
+    constructor(name = "", resource = 0, duration = 0, nextActivity = "") {
         this.name = "";
         this.description = "";
         this.resource = 0;
@@ -13,24 +29,48 @@ class Activity {
         //Has to solve activity issue
         //prevActivity:Activity = new Activity();
         //nextActivity: Array<Activity> = [];
+        this.prevActivity = "";
+        this.nextActivity = "";
         this.earlyStart = 0;
         this.earlyFinish = 0;
         this.lateStart = 0;
         this.lateFinish = 0;
         this.totalFloat = 0;
-        this.name = name.toLowerCase();
+        this.name = name.toUpperCase();
         this.duration = duration;
         this.resource = resource;
+        this.nextActivity = nextActivity.toUpperCase();
     }
 }
-var a = new Activity("A", 3, 3);
-let str = "sd a  dfs sdf ";
-try {
-    // var data = fs.readFileSync("F:\\IIT 8th Semester\\Software Project Management\\Group Assingments\\Project 1\\src\\input.txt", 'utf8');
-    var data = fs_1.default.readFileSync("src\\input.txt", "utf8");
-    console.log(data);
+const readline = require("readline");
+function processLineByLine() {
+    var e_1, _a;
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // var data = fs.readFileSync("F:\\IIT 8th Semester\\Software Project Management\\Group Assingments\\Project 1\\src\\input.txt", 'utf8');
+            const fileStream = fs_1.default.createReadStream("src\\input.txt", "utf8");
+            const rl = readline.createInterface({
+                input: fileStream,
+                crlfDelay: Infinity,
+            });
+            try {
+                for (var rl_1 = __asyncValues(rl), rl_1_1; rl_1_1 = yield rl_1.next(), !rl_1_1.done;) {
+                    const line = rl_1_1.value;
+                }
+            }
+            catch (e_1_1) { e_1 = { error: e_1_1 }; }
+            finally {
+                try {
+                    if (rl_1_1 && !rl_1_1.done && (_a = rl_1.return)) yield _a.call(rl_1);
+                }
+                finally { if (e_1) throw e_1.error; }
+            }
+            //console.log(data);
+        }
+        catch (e) {
+            console.log("Error:", e.stack);
+        }
+    });
 }
-catch (e) {
-    console.log("Error:", e.stack);
-}
+processLineByLine();
 //# sourceMappingURL=start.js.map
--- src/start.ts
+++ src/start.ts
@@ -10,29 +10,45 @@ class Activity {
     //Has to solve activity issue
     //prevActivity:Activity = new Activity();
     //nextActivity: Array<Activity> = [];
+    prevActivity: string = "";
+    nextActivity: string = "";
     earlyStart: number = 0;
     earlyFinish: number = 0;
     lateStart: number = 0;
     lateFinish: number = 0;
     totalFloat: number = 0;
 
-    constructor(name: string = "", resource: number = 0, duration: number = 0) {
-        this.name = name.toLowerCase();
+    constructor(
+        name: string = "",
+        resource: number = 0,
+        duration: number = 0,
+        nextActivity: string = ""
+    ) {
+        this.name = name.toUpperCase();
         this.duration = duration;
         this.resource = resource;
+        this.nextActivity = nextActivity.toUpperCase();
     }
 }
 
-var a = new Activity("A", 3, 3);
+const readline = require("readline");
 
-let str = "sd a  dfs sdf ";
+async function processLineByLine() {
+    try {
+        // var data = fs.readFileSync("F:\\IIT 8th Semester\\Software Project Management\\Group Assingments\\Project 1\\src\\input.txt", 'utf8');
+        const fileStream = fs.createReadStream("src\\input.txt", "utf8");
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity,
+        });
 
-try {
-    // var data = fs.readFileSync("F:\\IIT 8th Semester\\Software Project Management\\Group Assingments\\Project 1\\src\\input.txt", 'utf8');
-    var data = fs.readFileSync("src\\input.txt", "utf8");
-    console.log(data);
-    
-
-} catch (e) {
-    console.log("Error:", e.stack);
+        for await (const line of rl) {
+            // console.log(`Line from file: ${line}`);
+        }
+        //console.log(data);
+    } catch (e) {
+        console.log("Error:", e.stack);
+    }
 }
+
+processLineByLine();
